be4a35872e71a3b7219ea7abf0b3442838859899
Su Yatanar <suyatanar86@gmail.com>
2022-04-07 14:24:47 +0800

    Create common header blade

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,5 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Get Data</title>
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" src="{{ asset('js/main.js') }}"></script>
 </head>
 <body>
